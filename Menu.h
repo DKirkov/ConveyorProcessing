dcbd518c7421604adb9fd5aeb7eceb51be73c9fa
DKirkov <98287355+DKirkov@users.noreply.github.com>
2022-01-27 15:53:38 +0200

    start menu

---
+++ Menu.h
@@ -0,0 +1,19 @@
+/**
+*
+* Solution to course project # 1
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2021/2022
+*
+* @author Dimitar Kirkov
+* @idnumber 4MI0600002
+* @compiler VC
+*
+* <file with the start menu>
+*
+*/
+
+#pragma once
+void Menuformatting();
+void Optionsformatting();
+void Menu();
