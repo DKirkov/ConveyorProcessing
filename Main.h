864902c35082309f31b5444399c57f0fb196fc69
DKirkov <98287355+DKirkov@users.noreply.github.com>
2022-01-27 15:59:03 +0200

    function for choosing an option from 1 to 8

---
+++ Main.h
@@ -0,0 +1,18 @@
+/**
+*
+* Solution to course project # 1
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2021/2022
+*
+* @author Dimitar Kirkov
+* @idnumber 4MI0600002
+* @compiler VC
+*
+* <file with function for choosing an option from 1 to 8>
+*
+*/
+
+#pragma once
+using namespace std;
+void Main(int option);
