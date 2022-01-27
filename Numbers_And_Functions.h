dcf67d05a53998ebffc8f1469fa208164cd733e2
DKirkov <98287355+DKirkov@users.noreply.github.com>
2022-01-27 15:47:45 +0200

    get the numbers and the functions from text files

---
+++ Numbers_And_Functions.h
@@ -0,0 +1,21 @@
+/**
+*
+* Solution to course project # 1
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2021/2022
+*
+* @author Dimitar Kirkov
+* @idnumber 4MI0600002
+* @compiler VC
+*
+* <file with functions to get the numbers and functions from the files>
+*
+*/
+
+#pragma once
+using namespace std;
+int getNumberOfNumbers();
+int getNumberOfFunctions();
+void getNumbers(double*& numbers);
+void getFunctions(string*& functions);
