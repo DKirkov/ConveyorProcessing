07c294a5c5c39f4d6c8cf47e327e1e8773be5d7d
DKirkov <98287355+DKirkov@users.noreply.github.com>
2022-01-27 15:50:17 +0200

    functions for options from 1 to 4

---
+++ Options_1_to_4.h
@@ -0,0 +1,23 @@
+/**
+*
+* Solution to course project # 1
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2021/2022
+*
+* @author Dimitar Kirkov
+* @idnumber 4MI0600002
+* @compiler VC
+*
+* <file with functions for options from 1 to 4>
+*
+*/
+
+#pragma once
+using namespace std;
+void viewInputNumbers(double* numbers, int NumberOfNumbers);
+void saveNewNumbersToTheFile(double* numbers, int numberOfNumbers);
+void changeOneNumber(double* numbers, int numberOfNumbers);
+void viewInputFunctions(string* functions, int numberOfFunctions);
+void saveNewFunctionsToTheFile(string* functions, int numberOfFunctions);
+void changeOneFunction(string* functions, int numberOfFunctions);
